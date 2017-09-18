82de4e9d8d94980a48995d144fd38e51c9daf35f
jerrywham <github@ecyseo.net>
2017-09-18 17:01:35 +0200

    Create fr-help.php

---
+++ lang/fr-help.php
@@ -0,0 +1,3 @@
+<p>Pour que le plugin fonctionne, il faut supprimer ou mettre en commentaire, la ligne de vérification du captcha par défaut dans votre thème.</p>
+<p>Dans le thème par défaut, cela concerne la ligne +/- 80 du fichier commentaires.php, située après <code>&gt;?php $plxShow->capchaQ(); ?&lt;</code>
+<code>&gt;input id="id_rep" name="rep" type="text" size="2" maxlength="1" style="width: auto; display: inline;" /&lt;</code>
